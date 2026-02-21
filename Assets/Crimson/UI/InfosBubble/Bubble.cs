46c3328bbca3a939468fed2132f4856d02d9dffb
Erkenoss <xavier.bertin6@gmail.com>
2026-02-21 23:36:39 +0100

    feat: last push to finish it

--- Assets/Crimson/UI/InfosBubble/Bubble.cs
+++ Assets/Crimson/UI/InfosBubble/Bubble.cs
@@ -20,51 +20,9 @@ namespace Crimson.UI
         [SerializeField]
         private GameObject bubble = null;
 
-        [Tooltip("Canvas where the bubble is")]
-        [SerializeField]
-        private Canvas canva = null;
-
-        [Tooltip("Rect transform of the bubble")]
-        [SerializeField]
-        private RectTransform bubbleRect = null;
-
-        [Tooltip("OffSet we want to add on the mouse to avoid image cover the mouse")]
-        [SerializeField]
-        private Vector2 offSet = Vector2.zero;
-
-        /// <summary>
-        /// Use to check where is the mouse on the canva
-        /// </summary>
-        private Vector2 mousePosition = Vector2.zero;
-
-        /// <summary>
-        /// It's the position of the anchored of the moouse on the canva
-        /// </summary>
-        private Vector2 anchorPosition = Vector2.zero;
-
         #endregion
 
         #region MonoBehaviour Callbacks
-
-        private void LateUpdate()
-        {
-            if (canva == null || bubbleRect == null)
-            {
-                return;
-            }
-
-            mousePosition = Mouse.current.position.ReadValue();
-
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canva.transform as RectTransform,
-                mousePosition,
-                canva.renderMode == RenderMode.ScreenSpaceOverlay ? null : canva.worldCamera,
-                out anchorPosition);
-
-            anchorPosition += offSet;
-            bubbleRect.anchoredPosition = anchorPosition;
-        }
-
         #endregion
 
         #region Public Methods
@@ -78,11 +36,6 @@ namespace Crimson.UI
             if (model == null || bubbleText == null)
             {
                 return;
-            }
-
-            if (!bubble.activeSelf)
-            {
-                bubble.SetActive(true);
             }
 
             if (string.IsNullOrEmpty(model.Description))
